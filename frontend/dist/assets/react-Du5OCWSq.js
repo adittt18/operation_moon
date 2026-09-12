f784b809ab74088ad384548150096a0edb5a924a
adittt18 <adittt18@users.noreply.github.com>
2026-09-12 18:06:36 +0530

    feat: add Fit to Screen vs Expand option for result images and reduce modal opacity to 69%

--- frontend/dist/assets/react-Du5OCWSq.js
+++ frontend/dist/assets/react-Du5OCWSq.js
@@ -1,4 +1,4 @@
-import{r as _a}from"./icons-BPtnLD4X.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{r as _a}from"./icons-Bcou7imM.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
